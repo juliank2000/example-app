dacfc09a7e05727d19555e32db2ad79e3aedf081
juliank2000 <barriosjulian246@gmail.com>
2023-06-26 23:03:46 -0500

    first commit

---
+++ resources/views/partials/perfil.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid">
+    <h1>PERFIL</h1>  
+    
+ <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium enim tempore consequatur voluptatem a aspernatur praesentium nam aut ea ullam fuga facilis mollitia atque, voluptatum id dignissimos asperiores eos officia.</h2>
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
